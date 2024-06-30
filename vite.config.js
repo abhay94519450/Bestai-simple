ce2dfb533eac04eed0a9a9ebcb448b88ab978a24
abhay94519450 <102897632+abhay94519450@users.noreply.github.com>
2024-06-30 10:35:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+// voice 
+export default {
+  build: {
+    rollupOptions: {
+      external: ['react-speech-recognition']
+    }
+  }
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
